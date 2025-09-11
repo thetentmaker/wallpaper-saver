c1d42847679b0a001e8a4eb790bb6f896a690eb7
tentmaker <ovsoc@daum.net>
2025-09-11 11:36:05 +0900

    feat: ImageDetailScreen 구현

--- app/src/screen/ImageDetailScreen.tsx
+++ app/src/screen/ImageDetailScreen.tsx
@@ -1,13 +1,27 @@
-import { StyleSheet, View } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 import { Header } from "../designsystem/Header";
+import { RemoteImage } from "../designsystem/RemoteImage";
+import { RootStackNavigationProp } from "../navigation/\bRootStackNavigation";
 
 const ImageDetailScreen = () => {
-return (
+  const navigation = useNavigation<RootStackNavigationProp>();
+  const route = useRoute();
+  const imageUrl = route.params as { imageUrl: string };
+  const { width } = useWindowDimensions();
+  return (
     <View style={styles.container}>
       <Header>
-        <Header.Icon name="arrow-back" />
+        <Header.Icon name="arrow-back" onPress={() => navigation.goBack()} />
         <Header.Title>IMAGE DETAIL</Header.Title>
       </Header>
+      <View style={styles.content}>
+        <RemoteImage
+          uri={imageUrl.imageUrl}
+          width={width * 0.98}
+          height={width * 1.5}
+        />
+      </View>
     </View>
   );
 };
@@ -17,5 +31,9 @@ export default ImageDetailScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
   },
 });
